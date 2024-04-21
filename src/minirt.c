c1a1d87cb6b8f7985e52acf4d1244fc940831bcd
Jaqueline Ribeiro Inocencio <jaqribei@student.42sp.org.br>
2024-04-21 00:16:17 -0300

    start mlx window

--- src/minirt.c
+++ src/minirt.c
@@ -6,7 +6,7 @@
 /*   By: jaqribei <jaqribei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 11:36:10 by jaqribei          #+#    #+#             */
-/*   Updated: 2024/04/18 16:02:51 by jaqribei         ###   ########.fr       */
+/*   Updated: 2024/04/21 00:15:22 by jaqribei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 
 int	main(int argc, char **argv)
 {
+	mlx_t	*mlx;
+
 	check_args(argc, argv);
+	mlx = mlx_init(800, 800, "miniRT", 0);
+	mlx_loop(mlx);
 	return (0);
 }
